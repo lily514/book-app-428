b7015284e282ec920622ba71a86e4c5d18abb0d1
Master-Owl <cheesytrevor@gmail.com>
2018-02-26 13:38:48 -0700

    Created layout for cards to use

---
+++ app/src/main/java/com/cs428/app/bookapp/adapter/CardPresenter.java
@@ -0,0 +1,9 @@
+package com.cs428.app.bookapp.adapter;
+
+/**
+ * Created by chees on 2/26/2018.
+ */
+// http://android.xsoftlab.net/training/tv/playback/card.html
+public class CardPresenter {
+
+}
