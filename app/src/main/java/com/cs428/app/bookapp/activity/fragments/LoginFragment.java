9646b418c5ec2dd050e9966b38c844f9a5aeb3a0
Unknown <taetem@byu.edu>
2018-03-12 13:37:23 -0600

    login fragment

---
+++ app/src/main/java/com/cs428/app/bookapp/activity/fragments/LoginFragment.java
@@ -0,0 +1,49 @@
+package com.cs428.app.bookapp.activity.fragments;
+
+
+public class LoginFragment extends Fragment {
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.login, container, false);
+
+
+        // Inflate the layout for this fragment
+
+        TextView registerScreen = (TextView) rootView.findViewById(R.id.link_to_register);
+
+        // Listening to register new account link
+        registerScreen.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                // Switching to Register screen
+                Intent i = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(i);
+            }
+        });
+
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
