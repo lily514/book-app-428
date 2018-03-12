9d2bc9106fba0455f49c4fa93d1dec43e96f1a71
Michael Gardanier <mgardanier@gmail.com>
2018-03-12 15:28:42 -0600

    fixed unit test errors.

---
+++ app/src/main/java/com/cs428/app/bookapp/networking/mock/Mock_ServerCommunicator.java
@@ -0,0 +1,98 @@
+package com.cs428.app.bookapp.networking.mock;
+
+import com.cs428.app.bookapp.interfaces.IServerCommunicator;
+import com.cs428.app.bookapp.model.Book;
+import com.cs428.app.bookapp.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by mgard on 3/10/2018.
+ *
+ *  THIS CLASS IS FOR TESTING PURPOSES ONLY!!!
+ */
+
+public class Mock_ServerCommunicator implements IServerCommunicator {
+    @Override
+    public List<User> getUsers() {
+        List<User> users = new ArrayList<>();
+        for(int i = 0; i < 100; i++) {
+            User u = new User("user" + i, Integer.toString(i));
+            users.add(u);
+        }
+        return users;
+    }
+
+    @Override
+    public User getUser(String id) {
+        return null;
+    }
+
+
+
+    @Override
+    public List<User> getFriends(String id) {
+        List<User> users = new ArrayList<>();
+        for(int i = 0; i < 50; i++) {
+            User u = new User("user" + i, Integer.toString(i));
+            users.add(u);
+        }
+        return users;
+    }
+
+    @Override
+    public List<Book> getUsersFriendsReadingList(String id) {
+        List<Book> books = new ArrayList<>();
+        for(int i = 0; i < 500; i++) {
+            Book b = new Book("book" + i, "author" + i, Integer.toString(i),null);
+            books.add(b);
+        }
+        return books;
+    }
+
+    @Override
+    public User login(String username, String password) {
+        return new User(username, username);
+    }
+
+    @Override
+    public boolean registerUser(User user) {
+        return true;
+    }
+
+    @Override
+    public boolean addRecommendation(String id, Book book) {
+        return true;
+    }
+
+    @Override
+    public boolean addToReadingList(String id, Book book) {
+        return true;
+    }
+
+    @Override
+    public boolean followUser(String myId, String otherId) {
+        return true;
+    }
+
+    @Override
+    public List<Book> searchForBook(String searchString) {
+        List<Book> books = new ArrayList<>();
+        for(int i = 0; i < 20; i++) {
+            Book b = new Book("book" + i, "author" + i, Integer.toString(i),null);
+            books.add(b);
+        }
+        return books;
+    }
+
+    @Override
+    public Book getBookById(String id) {
+        return new Book("book", "author", id, null);
+    }
+
+    @Override
+    public boolean rateBook(String userId, String bookId, int rating) {
+        return true;
+    }
+}
---
+++ app/src/test/java/com/cs428/app/bookapp/networking/ServerProxyTest.java
@@ -0,0 +1,57 @@
+package com.cs428.app.bookapp.networking;
+
+import com.cs428.app.bookapp.interfaces.IServerCommunicator;
+import com.cs428.app.bookapp.interfaces.IServerProxy;
+import com.cs428.app.bookapp.model.Book;
+import com.cs428.app.bookapp.model.User;
+import com.cs428.app.bookapp.networking.mock.Mock_ServerCommunicator;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by mgard on 3/10/2018.
+ */
+public class ServerProxyTest {
+
+    private IServerProxy proxy;
+
+    @Before
+    public void setup() {
+        IServerCommunicator com = new Mock_ServerCommunicator();
+        proxy = new ServerProxy(com);
+    }
+
+    @Test
+    public void login() throws Exception {
+        User test = proxy.login("test", "test");
+        assertEquals("test", test.getName());
+
+        User test1 = proxy.login("test1", "test1");
+        assertEquals("test1", test1.getName());
+    }
+
+    @Test
+    public void searchBook() throws Exception {
+        List<Book> results = proxy.searchBook("testTerm");
+
+        for(int i = 0; i < 20; i++) {
+            Book b = results.get(i);
+            assertEquals("book" + i, b.getName());
+            assertEquals("author" + i, b.getAuthor());
+            assertEquals(Integer.toString(i), b.getIsbn());
+        }
+
+    }
+
+    /*@Test
+    public void getRecommendationFor() throws Exception {
+
+    }
+    */
+
+}
