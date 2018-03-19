f9b520b2234dfe5d9dfb56898c39d4ea701e2416
Michael Gardanier <mgardanier@gmail.com>
2018-03-19 14:56:10 -0600

    Added in deserializers for book and user. Also added fields to Book and User classes that didnt match what the backend had.

--- frontend/book-app-428/app/src/main/java/com/cs428/app/bookapp/networking/Serializer.java
+++ frontend/book-app-428/app/src/main/java/com/cs428/app/bookapp/networking/Serializer.java
@@ -2,7 +2,12 @@ package com.cs428.app.bookapp.networking;
 
 import com.cs428.app.bookapp.model.Book;
 import com.cs428.app.bookapp.model.User;
+import com.cs428.app.bookapp.networking.customSerializers.BookDeserializer;
+import com.cs428.app.bookapp.networking.customSerializers.UserDeserializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -18,7 +23,17 @@ public class Serializer {
     }
 
     public User deserializeUser(String jsonUser) {
-        return null;
+        ObjectMapper mapper = new ObjectMapper();
+        SimpleModule module = new SimpleModule();
+        module.addDeserializer(User.class, new UserDeserializer());
+        mapper.registerModule(module);
+
+        try {
+            return mapper.readValue(jsonUser, User.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     public String serializeBook(Book book) {
@@ -26,7 +41,17 @@ public class Serializer {
     }
 
     public Book deserializeBook(String jsonBook) {
-        return null;
+        ObjectMapper mapper = new ObjectMapper();
+        SimpleModule module = new SimpleModule();
+        module.addDeserializer(Book.class, new BookDeserializer());
+        mapper.registerModule(module);
+
+        try {
+            return mapper.readValue(jsonBook, Book.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     public List<Book> deserializeListOfBooks(String jsonList) {
